e99176c9fddea8bb41349aa302659959b4f704a2
Steven Yi <stevenyi@gmail.com>
2013-11-15 12:37:17 -0500

    Modified OSCsound.py to require taking csd_file from commandline and use optional --port setting, used argparse to give better commandline info

--- OSCsound.py
+++ OSCsound.py
@@ -1,81 +1,103 @@
 from csnd6 import Csound, CsoundPerformanceThread, csoundInitialize
 from OSC import OSCServer
 from time import sleep
+import argparse
+import types
 
 # setup csound
-csoundInitialize(1)
-orc = """
-sr=44100
-ksmps=32
-nchnls=2
-0dbfs=1
+csoundInitialize(3)
+csound = None
+csPerfThread = None
 
-instr 1 
-aout vco2 0.5, 440
-outs aout, aout
-endin"""
+def csound_start(csd_file):
+    global csound 
+    global csPerfThread
 
+    csound = Csound()
+    csound.SetOption("-odac")  
+    csound.Compile(csd_file)     
+    csound.Start()  
+    csPerfThread = CsoundPerformanceThread(csound)
+    csPerfThread.Play()
 
-# Defining our Csound SCO code 
-sco = "i1 0 1"
-
-cs = Csound()
-cs.SetOption("-odac")  # Using SetOption() to configure Csound
-                      # Note: use only one commandline flag at a time
-
-cs.CompileOrc(orc)     # Compile the Csound Orchestra string
-#cs.ReadScore(sco)      # Compile the Csound SCO String
-cs.Start()  # When compiling from strings, this call is necessary before doing any performing
-pt = CsoundPerformanceThread(cs)
-pt.Play()
+def csound_stop():
+    global csPerfThread
+    global csound
+    csPerfThread.Stop()
+    csPerfThread.Join()
+    csound.Stop()
 
 ###
 
-server = OSCServer ( ("localhost", 7110))
-server.timeout = 0
+server = None
 run = True
-
 
 def handle_timeout(self):
     self.timed_out = True
 
-import types
-server.handle_timeout = types.MethodType(handle_timeout, server)
-
-
 def handle_score(path, tags, args, source):
+    global csPerfThread
     #print "TESTING", args[0]
-    pt.InputMessage(args[0])
+    csPerfThread.InputMessage(args[0])
 
 def handle_cc(path, tags, args, source):
+    global csound
     if(len(args) != 2):
         print "Error: CC must have two args: channelName floatValue"
         return
     #print "TESTING: %s %s"%(args[0], args[1])
-    cs.SetChannel(args[0], args[1])
+    csound.SetChannel(args[0], args[1])
 
 def quit_callback(path, tags, args, source):
     global run
     run = False
     print "Quitting Server..."
 
-server.addMsgHandler("/sco", handle_score)
-server.addMsgHandler("/cc", handle_cc)
-server.addMsgHandler("/quit", quit_callback)
 
 def each_frame():
+    global server
     server.timed_out = False
     while not server.timed_out:
         server.handle_request()
 
-print "Starting OSC Server..."
-while run:
-    sleep(1)
-    each_frame()
 
-server.close()
-pt.Stop()
-pt.Join()
-cs.Stop()
+def server_start(port=7110):
+    global server
+    server = OSCServer ( ("localhost", 7110))
+    server.timeout = 0
+
+    server.handle_timeout = types.MethodType(handle_timeout, server)
+
+    server.addMsgHandler("/sco", handle_score)
+    server.addMsgHandler("/cc", handle_cc)
+    server.addMsgHandler("/quit", quit_callback)
+
+def server_stop():
+    global server 
+    server.close()
 
 
+
+
+def main():
+    
+    parser = argparse.ArgumentParser(prog='OSCsound', 
+                                     description='OSCsound - version 0.1')
+
+    parser.add_argument("csd_file", help="Csound CSD File to run")
+    parser.add_argument("-p", "--port", help="Server Port, defaults to 7110", type=int, default=7110)
+
+    args = parser.parse_args()
+
+    csound_start(args.csd_file)
+    server_start(args.port)
+
+    while run:
+        sleep(1)
+        each_frame()
+
+    csound_stop()
+    server_stop()
+
+if __name__ == '__main__':
+    main()
---
+++ test.csd
@@ -0,0 +1,16 @@
+<CsoundSynthesizer>
+<CsInstruments>
+
+sr=44100
+ksmps=32
+nchnls=2
+0dbfs=1
+
+instr 1 
+aout vco2 0.5, 440
+outs aout, aout
+endin
+
+</CsInstruments>
+
+</CsoundSynthesizer>
